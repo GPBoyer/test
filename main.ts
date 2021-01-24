51b6f96c48a39650683acd658cb65ac6a0e7027b
GPBoyer <67514487+GPBoyer@users.noreply.github.com>
2021-01-24 10:39:41 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,6 @@ basic.forever(function () {
     basic.showString("'Bonjour'")
     basic.pause(2000)
     basic.showNumber(input.temperature())
+    basic.showString("degré C")
     basic.pause(2000)
 })
